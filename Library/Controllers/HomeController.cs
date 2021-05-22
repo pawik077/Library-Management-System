f420956299350fe6c6dfc5f520e837c2e7a8cdd1
pawik077 <pawik.r.s.077@gmail.com>
2021-05-23 00:35:16 +0200

    Added DbContext to HomeController

--- Library/Controllers/HomeController.cs
+++ Library/Controllers/HomeController.cs
@@ -1,18 +1,16 @@
 ﻿using Library.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Library.Controllers {
 	public class HomeController : Controller {
 		private readonly ILogger<HomeController> _logger;
+		private readonly BibliotekaContext _db;
 
-		public HomeController(ILogger<HomeController> logger) {
+		public HomeController(ILogger<HomeController> logger, BibliotekaContext db) {
 			_logger = logger;
+			_db = db;
 		}
 
 		public IActionResult Index() {
