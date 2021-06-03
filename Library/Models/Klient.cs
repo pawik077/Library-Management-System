52524146ee3ee1f0fa250912f46244b53933371a
pawik077 <pawik.r.s.077@gmail.com>
2021-06-03 18:24:19 +0200

    Added Required annotations for Klient class

--- Library/Models/Klient.cs
+++ Library/Models/Klient.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.ComponentModel.DataAnnotations;
 #nullable disable
 
 namespace Library.Models
@@ -11,14 +11,18 @@ namespace Library.Models
         {
             Wypożyczenia = new HashSet<Wypożyczenie>();
         }
-
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string ImięKlienta { get; set; }
+        [Required]
         public string NazwiskoKlienta { get; set; }
         public string Pesel { get; set; }
+        [Required]
         public string AdresZamieszkania { get; set; }
         public string NumerTelefonu { get; set; }
         public string AdresEmail { get; set; }
+        [Required]
         public DateTime DataRejestracji { get; set; }
 
         public virtual ICollection<Wypożyczenie> Wypożyczenia { get; set; }
