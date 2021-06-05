898e14586b5f82eb5471e1ba9f3c61f074f82d9e
pawik077 <pawik.r.s.077@gmail.com>
2021-06-05 14:32:35 +0200

    Added book creation

--- Library/Controllers/FindBooksController.cs
+++ Library/Controllers/FindBooksController.cs
@@ -24,8 +24,33 @@ namespace Library.Controllers {
 			var BooksViewModel = _db.FindBookModels.FromSqlRaw<FindBook>(sql);
 			return View(BooksViewModel);
 		}
-		public IActionResult Create() {
-			return View();
+		public IActionResult Create(FindBook książka) {
+			if(książka.Tytuł == null)
+				return View();
+			else {
+				int? IDAutor, IDGatunek;
+				if((IDAutor = _db.Autorzy.SingleOrDefault(a => a.ImięAutora == książka.Imię_Autora && a.NazwiskoAutora == książka.Nazwisko_Autora)?.Id) == null) {
+					_db.Autorzy.Add(new Autor { ImięAutora = książka.Imię_Autora, NazwiskoAutora = książka.Nazwisko_Autora });
+					_db.SaveChanges();
+					IDAutor = _db.Autorzy.SingleOrDefault(a => a.ImięAutora == książka.Imię_Autora && a.NazwiskoAutora == książka.Nazwisko_Autora)?.Id;
+				}
+				if((IDGatunek = _db.Gatunki.SingleOrDefault(g => g.NazwaGatunku == książka.Nazwa_Gatunku)?.Id) == null) {
+					_db.Gatunki.Add(new Gatunek { NazwaGatunku = książka.Nazwa_Gatunku });
+					_db.SaveChanges();
+					IDGatunek = _db.Gatunki.SingleOrDefault(g => g.NazwaGatunku == książka.Nazwa_Gatunku)?.Id;
+				}
+				_db.Książki.Add(new Książka {
+					Tytuł = książka.Tytuł,
+					Idautor = IDAutor.Value,
+					Idgatunek = IDGatunek.Value,
+					Opis = książka.Opis,
+					Okładka = książka.Okładka,
+					Stan = książka.Stan,
+					Dostępność = książka.Stan
+				});
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
 		}
 	}
 }
