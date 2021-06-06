1e40a8f1772d81af43c6029b81cad70d70d31e8a
pawik077 <pawik.r.s.077@gmail.com>
2021-06-06 18:25:18 +0200

    Improved data display for clients

--- Library/Models/Klient.cs
+++ Library/Models/Klient.cs
@@ -17,14 +17,29 @@ namespace Library.Models
         public string ImięKlienta { get; set; }
         [Required]
         public string NazwiskoKlienta { get; set; }
+        [Display(Name = "PESEL")]
         public string Pesel { get; set; }
-        [Required]
+        [Required, Display(Name = "Adres zamieszkania")]
         public string AdresZamieszkania { get; set; }
+        [Display(Name = "Numer telefonu")]
         public string NumerTelefonu { get; set; }
+        [Display(Name = "Adres e-mail")]
         public string AdresEmail { get; set; }
         [Required]
         public DateTime DataRejestracji { get; set; }
 
         public virtual ICollection<Wypożyczenie> Wypożyczenia { get; set; }
+        [Display(Name = "Data rejestracji")]
+        public string ShortDate {
+            get {
+                return DataRejestracji.ToString("d");
+			}
+		}
+        [Display(Name = "Imię i nazwisko")]
+        public string NameConcatenation {
+            get {
+                return ImięKlienta + " " + NazwiskoKlienta;
+			}
+		}
     }
 }
